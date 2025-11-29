16d70fd16a52cc7510780c7267dc6be88eede753
Vladimer Gabisonia <v.gabisonia@andersenlab.com>
2025-11-29 19:29:41 +0400

    feat: configure redux in application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import Products from "./pages/Products/Products";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+          <Route path="products" element={<Products />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
