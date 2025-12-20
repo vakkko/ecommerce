0603c75e13f19b00f5937f594c241d40d3691520
Vladimer Gabisonia <v.gabisonia@andersenlab.com>
2025-12-20 23:32:29 +0400

    feat: add nested product route

--- src/pages/Products/ProductsList/ProductsList.tsx
+++ src/pages/Products/ProductsList/ProductsList.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-import Product from "./Product/Product";
+import ProductItem from "./ProductItem/ProductItem";
 
 import type { ProductsListProps } from "./productsList.types";
 
 import { ProductsListContainer } from "./products.styled";
+import { Link } from "react-router";
 
 export default function ProductsList({ data }: ProductsListProps) {
   return (
@@ -12,12 +13,14 @@ export default function ProductsList({ data }: ProductsListProps) {
       {data &&
         data.data.map((product) => (
           <React.Fragment key={product.id}>
-            <Product
-              cover_image={product.cover_image}
-              name={product.name}
-              price={product.price}
-              description={product.description}
-            />
+            <Link to={String(product.id)}>
+              <ProductItem
+                cover_image={product.cover_image}
+                name={product.name}
+                price={product.price}
+                description={product.description}
+              />
+            </Link>
           </React.Fragment>
         ))}
     </ProductsListContainer>
