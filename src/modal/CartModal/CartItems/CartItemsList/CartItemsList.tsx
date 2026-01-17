cb24676f75a0a39e827cc6d7da925140107d2fe3
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-01-17 15:02:34 +0400

    refactor: extract PriceRemove from CartItemsList component

--- src/modal/CartModal/CartItems/CartItemsList/CartItemsList.tsx
+++ src/modal/CartModal/CartItems/CartItemsList/CartItemsList.tsx
@@ -1,33 +1,19 @@
 import { useState } from "react";
 
+import PriceRemove from "./PriceRemove/PriceRemove";
 import ItemImage from "./ItemImage/ItemImage";
 
-import {
-  useDeleteCartItemMutation,
-  useGetCartItemsQuery,
-} from "../../../../store/services/cartApi/cartApi";
-
 import type { CartItem } from "../../../../store/services/cartApi/cartApi.types";
-import type { DeleteItemParams } from "./cartItemsList.types";
 
 import {
   ItemDetails,
   ItemDetailsContainer,
   ItemsList,
-  PriceRemove,
 } from "./cartItemsList.styled";
 import ItemDescription from "./ItemDescription/ItemDescription";
 import Quantity from "./Quantity/Quantity";
 
 export default function CartItemsList({ data }: { data: CartItem[] }) {
-  const [deleteItem] = useDeleteCartItemMutation();
-  const { refetch } = useGetCartItemsQuery();
-
-  const handleItemDelete = ({ id, color, size }: DeleteItemParams) => {
-    deleteItem({ id, color, size });
-    refetch();
-  };
-
   const [items, setItems] = useState(data);
 
   return (
@@ -54,20 +40,12 @@ export default function CartItemsList({ data }: { data: CartItem[] }) {
                 size={item.size}
               />
             </ItemDetails>
-            <PriceRemove>
-              <span>$ {item.price}</span>
-              <button
-                onClick={() =>
-                  handleItemDelete({
-                    id: item.id,
-                    color: item.color,
-                    size: item.size,
-                  })
-                }
-              >
-                Remove
-              </button>
-            </PriceRemove>
+            <PriceRemove
+              color={item.color}
+              id={item.id}
+              price={item.price}
+              size={item.size}
+            />
           </ItemDetailsContainer>
         </li>
       ))}
