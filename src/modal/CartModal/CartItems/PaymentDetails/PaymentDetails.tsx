109e733ba0d573cbfdec2a3f209eded1771269f1
Vladimer Gabisonia <v.gabisonia@andersenlab.com>
2026-01-29 15:08:51 +0400

    feat: add Congredulation modal on successfull payment

--- src/modal/CartModal/CartItems/PaymentDetails/PaymentDetails.tsx
+++ src/modal/CartModal/CartItems/PaymentDetails/PaymentDetails.tsx
@@ -1,57 +1,73 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
 import { Link } from "react-router";
 
-import type { PaymentDetailsProps } from "./paymentDetails.types";
-
-import { PaymentContainer } from "./paymentDetails.styled";
-
-import Button from "../../../../components/Button/Button";
 import {
   useFormContext,
   type FieldValues,
   type SubmitHandler,
 } from "react-hook-form";
 
+import Button from "../../../../components/Button/Button";
+import Congredulation from "../../../Congredulation/Congredulation";
+
 import { useCartCheckoutMutation } from "../../../../store/services/cartApi/cartApi";
+
+import type { PaymentDetailsProps } from "./paymentDetails.types";
+
+import { PaymentContainer } from "./paymentDetails.styled";
 
 export default function PaymentDetails({
   itemsSubtotal,
   handleClose,
   checkout,
 }: PaymentDetailsProps) {
+  const [paid, setPaid] = useState<boolean>(false);
+
   const delivery = 5;
   const total = delivery + itemsSubtotal;
 
   const { handleSubmit } = useFormContext() ?? "";
   const [payCheckout] = useCartCheckoutMutation();
 
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    payCheckout(data);
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    try {
+      await payCheckout(data).unwrap();
+      setPaid(true);
+    } catch (err) {
+      console.error("Chekcout failed", err);
+    }
   };
+
   return (
-    <PaymentContainer $checkout={checkout}>
-      <dl>
-        <div>
-          <dt>Items subtotal</dt>
-          <dd>{itemsSubtotal}</dd>
-        </div>
-        <div>
-          <dt>Delivery</dt>
-          <dd>{delivery}</dd>
-        </div>
-        <div>
-          <dt>Total</dt>
-          <dd>{total}</dd>
-        </div>
-      </dl>
-      {checkout ? (
-        <Button handleSubmit={handleSubmit(onSubmit)} type="submit">
-          Pay
-        </Button>
-      ) : (
-        <Link onClick={handleClose} to={"/checkout"}>
-          Go to checkout
-        </Link>
-      )}
-    </PaymentContainer>
+    <>
+      <PaymentContainer $checkout={checkout}>
+        <dl>
+          <div>
+            <dt>Items subtotal</dt>
+            <dd>{itemsSubtotal}</dd>
+          </div>
+          <div>
+            <dt>Delivery</dt>
+            <dd>{delivery}</dd>
+          </div>
+          <div>
+            <dt>Total</dt>
+            <dd>{total}</dd>
+          </div>
+        </dl>
+        {checkout ? (
+          <Button handleSubmit={handleSubmit(onSubmit)} type="submit">
+            Pay
+          </Button>
+        ) : (
+          <Link onClick={handleClose} to={"/checkout"}>
+            Go to checkout
+          </Link>
+        )}
+      </PaymentContainer>
+      {paid && createPortal(<Congredulation />, document.body)}
+    </>
   );
 }
---
+++ src/modal/Congredulation/Congredulation.tsx
@@ -0,0 +1,17 @@
+export default function Congredulation() {
+  return (
+    <div>
+      <button>
+        <img src="/images/close.svg" alt="close" />
+      </button>
+      <div>
+        <img src="/images/verified.png" alt="verified" />
+        <div>
+          <span>Congrats!</span>
+          <p>Your order is placed successfully!</p>
+        </div>
+        <button>Continue shopping</button>
+      </div>
+    </div>
+  );
+}
