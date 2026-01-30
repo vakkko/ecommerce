e436e9db3f3a337863f4d44f5fd78559d15c65ac
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-01-30 13:44:18 +0400

    feat: style congratulation modal and navigate to /products on continue/cancel actions

---
+++ src/modal/Congredulation/Congretulation.tsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router";
+
+import Button from "../../components/Button/Button";
+import HeadingText from "../../components/HeadingText/HeadingText";
+
+import { CongratsContainer, CongratsMessage } from "./congredulation.styles";
+
+export default function Congredulation() {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/products");
+  };
+
+  return (
+    <CongratsContainer>
+      <button onClick={handleNavigate}>
+        <img src="/images/close.svg" alt="close" />
+      </button>
+      <CongratsMessage>
+        <img src="/images/verified.png" alt="verified" />
+        <div>
+          <HeadingText text="Congrats!" />
+          <p>Your order is placed successfully!</p>
+        </div>
+        <Button handleSubmit={handleNavigate}>Continue shopping</Button>
+      </CongratsMessage>
+    </CongratsContainer>
+  );
+}
