2c0eba784de6d535accc7227d22d6de8fbf7b031
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-02-04 15:11:02 +0400

    fix: validation logic for password

--- src/validations/regex/regex.ts
+++ src/validations/regex/regex.ts
@@ -1,3 +1,3 @@
 export const USERNAME_VALIDATION = /^[A-Za-z0-9!@#$%^&*._-]+$/;
-export const PASSWORD_VALIDATION = /^\S{3,}$/;
+export const PASSWORD_VALIDATION = /^\S+$/;
 export const ADDRESS_VALIDATION = /\w/;
